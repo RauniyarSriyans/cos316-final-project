3332ca03a1e18ef805c7e1861eed344a8fc1b694
Christopher Speed <90000903+christopherspeed@users.noreply.github.com>
2023-12-13 15:03:00 -0500

    Basic data storage on the server via client

--- api/api.py
+++ api/api.py
@@ -1,14 +1,25 @@
 import time
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
+import json
 
 app = Flask(__name__)
+
+app.debug = True
 CORS(app)
+
+document = [] # global var
 
 @app.route('/')
 def index():
-    s = input("Send a message back!")
-    return {'data': s}
+    return {'data': json.dumps(document)}
+
+@app.route('/data', methods=['POST'])
+def data():
+  data = json.loads(request.data)
+  document.append(data["keycode"])
+  print(data, type(data))
+  return {'data': "Ello Guvnor"}
 
 @app.route('/time')
 def get_current_time():
